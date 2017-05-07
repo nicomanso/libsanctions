9f54ef2d04ed74dbcd2ca60daab6dfcb7de4dd63
Friedrich Lindenberg <friedrich@pudo.org>
2017-05-07 09:58:12 +0200

    initial checkin

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM pudo/scraper-base
+MAINTAINER Friedrich Lindenberg <friedrich@pudo.org>
+
+COPY . /opennames
+WORKDIR /opennames
+RUN pip install -q -r requirements.txt && pip install -q -e .
+CMD /bin/bash run.sh
---
+++ libsanctions/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+
+fmt = '[%(levelname)-8s] %(name)-12s: %(message)s'
+logging.basicConfig(level=logging.INFO, format=fmt)
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('alembic').setLevel(logging.WARNING)
+
+
+from libsanctions.source import Source  # noqa
